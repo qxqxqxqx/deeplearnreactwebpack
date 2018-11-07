36bd458f5fd0ef0ba182704119e004e99d602f6f
qiaoxin <qiaoxin@hiynn.com>
2018-11-07 15:50:58 +0800

    init

---
+++ app/index.js
@@ -0,0 +1,18 @@
+/*
+* @Author: qiaoxin
+* @Date:   2018/11/7 15:24
+* @Last Modified by:   qiaoxin
+* @Last Modified time: 2018/11/7 15:24
+* @Email: qiaoxinfc@gmail.com
+* @File Path:
+* @File Name: index
+* @Descript:
+*/
+import 'whatwg-fetch';
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+    <h1>just react</h1>,
+    document.querySelector('#App')
+)
